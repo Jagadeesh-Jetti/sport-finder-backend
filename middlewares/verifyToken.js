e71622a80d5cf81f0e9f5daad77bc7a44f8c09a8
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-03-09 17:04:56 +0530

    chore: updated packages

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  // Skip token verification for the default route
+  if (req.path === "/") {
+    return next();
+  }
+
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Token not provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+}
+
+module.exports = verifyToken;
